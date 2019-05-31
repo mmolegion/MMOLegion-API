663004fa156a85188304c2b1ed177c696cc7ae88
MMOLegion <dev@mmolegion.com>
2019-05-31 10:53:27 -0500

    update user

--- core/src/main/java/com/mmolegion/core/model/User.java
+++ core/src/main/java/com/mmolegion/core/model/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "user")
+@Table(name = "useraccounts")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 8696470445722242385L;
